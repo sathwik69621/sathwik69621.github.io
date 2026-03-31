1bbd81e953ccc07616b3abac2bcd0c26276fd4f9
Sathwik Uppu <uppusathwik1354@gmail.com>
2026-03-31 13:13:57 +0530

    Added new feature: user profile update

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute("href");
-        document.querySelector(targetId).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
